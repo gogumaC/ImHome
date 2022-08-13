655d7433699f729286e857399f29ab7a8c9d0e8c
jun <yoy06056@pusan.ac.kr>
2022-08-13 20:16:22 +0900

    login layout 개요

--- app/src/main/java/com/apptive/android/imhome/login/LoginFragment.kt
+++ app/src/main/java/com/apptive/android/imhome/login/LoginFragment.kt
@@ -1,6 +1,43 @@
 package com.apptive.android.imhome.login
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+import com.apptive.android.imhome.R
 import com.apptive.android.imhome.baseClass.BaseFragment
 
 class LoginFragment:BaseFragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val rootView = inflater.inflate(R.layout.fragment_login, container, false)
+
+        val id = rootView.findViewById<EditText>(R.id.idLogin)
+        val password = rootView.findViewById<EditText>(R.id.passwordLogin)
+        val buttonLogin = rootView.findViewById<Button>(R.id.buttonLoginLogin)
+        val buttonSignup = rootView.findViewById<Button>(R.id.buttonSignupLogin)
+
+        buttonLogin.setOnClickListener {
+
+
+        }
+
+        buttonSignup.setOnClickListener {
+
+        }
+
+
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
 }
